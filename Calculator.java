5930872253c6fb66a738b19168850e97d3354c4f
Amanze Ogbonna <darlingtonamz@gmail.com>
2015-09-18 20:53:55 +0000

    Added power function and editted average in Calculator.java

--- Calculator.java
+++ Calculator.java
@@ -1,4 +1,5 @@
 import java.util.Scanner;
+import java.util.ArrayList;
 public class Calculator{
 	public static Scanner in = new Scanner(System.in);
 	
@@ -17,28 +18,51 @@ public class Calculator{
 	public static void modulus(float first, float second){
 		System.out.println("Modulus Result of "+first+" & "+second+": " + (first % second));
 	}
-	public static void average(int first, int second){
-		System.out.println("Average Result of "+first+" & "+second+": " + ((first + second)/2));
+	public static void average(ArrayList<Integer> arr){
+		int n = arr.size();
+		int sum = 0;
+		for (int i=0; i < n; i++) {
+			sum += arr.get(i);
+		}
+		System.out.println("Average Result of "+arr.get(0)+" & "+arr.get(1)+": " + (sum/n));
 	}
 	public static void squareRoot(float first){
-		System.out.println("Square root Result of "+first+": " + (Math.sqrt(first)));
+		double check;
+		double result = first / 2 ;
+
+		do{
+			check = result;
+			result = (check + ( first / check)) /2;
+			System.out.println(check+" : "+result);
+		}while((check- result) != 0);
+		System.out.println("Square root Result of "+first+": " + result);
+	}
+	public static void power(float first, int second){
+		double pow = 1;
+		int i = second;
+		while(i > 0){
+			pow *= first;
+			i--;
+		}
+		System.out.println("Power Result of "+first+" to power of "+second+": " + pow);
 	}
 
 	public static void main (String[] args){
 		float a = 0,b = 0;
 		boolean done = false;
+		String mes = "";
 
 		while(!done){
 			done = true;
 			System.out.print("What to do? \n'1' - Add\n'2' - Subtract\n'3' - Multiply"
-			+"\n'4' - Divide\n'5' - Modulus\n'6' - Average\n'7' - Square root\nSelect one? ");
+			+"\n'4' - Divide\n'5' - Modulus\n'6' - Average\n'7' - Power\n'8' - Square root\nSelect one? ");
 			int resp = in.nextInt();
-			if (resp < 7) {
+			if (resp < 8) {
 				System.out.print("First number? ");
 				a = in.nextFloat();
 				System.out.print("Second number? ");
 				b = in.nextFloat();
-			}else if(resp == 7){
+			}else if(resp == 8){
 				System.out.print("Enter a number: ");
 				a = in.nextFloat();
 			}
@@ -54,16 +78,35 @@ public class Calculator{
 			}else if(resp == 5){
 				modulus(a,b);
 			}else if(resp == 6){
-				average(Math.round(a),Math.round(b));
+				if((a == Math.round(a)) && (b == Math.round(b))){
+					ArrayList<Integer> out = new ArrayList<Integer>();
+					out.add(Math.round(a));
+					out.add(Math.round(b));
+					average(out);
+				}else{
+					done = false;
+					mes = "Both numbers have to be integers from  '- 2,147,483,648 to  - 2,147,483,647";
+				}
+
 			}else if(resp == 7){
+				if(b == Math.round(b)){
+					power(a,Math.round(b));
+				}else{
+					done = false;
+					mes = "Your second input must be an integer";
+				}
+			}
+			else if(resp == 8){
 				squareRoot(a);
 			}else{
-				System.out.println("Wrong input!");
 				done = false;
 			}
+			if (!done)
+				System.out.println("Wrong input! \n"+mes);
 		}
 	}
 }
+
 /*•	add, a method that takes 2 numbers as arguments (not only integers) and prints their sum.
 •	subtract, a method that takes 2 numbers as arguments (not only integers) and prints their difference.
 •	divide, a method that takes 2 numbers as arguments (not only integers) and prints their quotient. This one is a little tricky. Let's see if you figure it out. That'd be awesome!
